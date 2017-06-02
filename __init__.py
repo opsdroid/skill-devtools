0c7e53fa79542febc3a945a74a24926523a3884d
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-06-02 14:53:08 +0100

    Reload

--- __init__.py
+++ __init__.py
@@ -1,16 +1,12 @@
 from opsdroid.matchers import match_regex
-import logging
-import random
 
-def setup(opsdroid):
-    logging.debug("Loaded hello module")
 
-@match_regex(r'hi|hello|hey|hallo')
-async def hello(opsdroid, config, message):
-    text = random.choice(["Hi {}", "Hello {}", "Hey {}"]).format(message.user)
-    await message.respond(text)
+@match_regex(r'reload')
+async def reload(opsdroid, config, message):
+    await message.respond("Reloading skills")
+    opsdroid.restart()
+    await message.respond("Reloaded")
 
-@match_regex(r'bye( bye)?|see y(a|ou)|au revoir|gtg|I(\')?m off')
-async def goodbye(opsdroid, config, message):
-    text = random.choice(["Bye {}", "See you {}", "Au revoir {}"]).format(message.user)
-    await message.respond(text)
+@match_regex(r'supertest')
+async def reloadtest(opsdroid, config, message):
+    await message.respond("Testytesttest")
